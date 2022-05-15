8ae852116330405bf26e049c05c4d5bd44376d03
lucasedbraga <braga.lucas@engenharia.ufjf.br>
2022-05-15 18:06:14 -0300

    leitura dos dados e formataçao para facilitar manipulação, mas ainda não está no formato ideal

--- plan_geracao/data_usi.py
+++ plan_geracao/data_usi.py
@@ -3,15 +3,14 @@ import pandas as pd
 def gerate_data(path='tab_ex.xlsx'):
     usis_data = pd.read_excel(path, engine='openpyxl')
     usis = []
+    #TODO melhorar leitura dos dados
     for i in range(len(usis_data)):
         usi = dict()
         leitura = usis_data.iloc[i, :]
         usi[leitura['Tipo']] = dict()
         for key in leitura.keys():
-            if key == 'Tipo':
-                pass
-            else:
-                usi[leitura['Tipo']][key] = leitura[key]
+            usi[leitura['Tipo']][key] = leitura[key]
+        usi = pd.DataFrame(usi)
         usis.append(usi)
 
     return usis
